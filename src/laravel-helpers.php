685ca160ff4baa6d212801a1590db5be2faf59be
skydiver <martulito@gmail.com>
2015-12-21 18:34:46 -0300

    pr function no longer return result, prints directly

--- src/laravel-helpers.php
+++ src/laravel-helpers.php
@@ -3,7 +3,7 @@
     if(!function_exists('pr')) {
         function pr($array=array()) {
             $text = '<pre>'.print_r($array, true).'</pre>';
-            return $text;
+            print $text;
         }
     }
 
