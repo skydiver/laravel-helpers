9f24fd0f102ed54b654dff702a6e32a27eef026c
Martin <skydiver@users.noreply.github.com>
2016-12-02 11:16:43 -0300

    * prd command * "documentation"

--- src/laravel-helpers.php
+++ src/laravel-helpers.php
@@ -1,10 +1,36 @@
 <?php
 
+    /**
+     * Print a PHP array with formated HTML
+     *
+     * @param array       $array The PHP array to print
+     *
+     * @return void
+     *
+     */
     if(!function_exists('pr')) {
-        function pr($array=array()) {
-            $text = '<pre>'.print_r($array, true).'</pre>';
-            print $text;
+
+        function pr($array=[]) {
+            print '<pre>'.print_r($array, true).'</pre>';
         }
+
+    }
+
+    /**
+     * Print a PHP array with formated HTML and stop execution
+     *
+     * @param array       $array The PHP array to print
+     *
+     * @return void
+     *
+     */
+    if(!function_exists('prd')) {
+
+        function prd($array=[]) {
+            print '<pre>'.print_r($array, true).'</pre>';
+            die();
+        }
+
     }
 
 ?>
